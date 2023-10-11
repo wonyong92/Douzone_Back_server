c7ef2d4d4f5b99cd00034bd6864c36c4bcbbf10b
wonyong92 <ska40806@gmail.com>
2023-10-11 16:12:04 +0900

    스프링 프로젝트 최초 구성

---
+++ src/main/java/com/example/bootproject/config/WebApplicationConfig.java
@@ -0,0 +1,10 @@
+package com.example.bootproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebApplicationConfig implements WebMvcConfigurer {
+
+}
