86fcea1bb58fea40307b6e302fb345af8b9a0b78
wonyong92 <ska40806@gmail.com>
2023-10-24 10:52:52 +0900

    컨트롤러 도메인 추가 + ip 분석을 위한 analyzer 유틸 생성

---
+++ src/main/java/com/example/bootproject/system/util/IpAnalyzer.java
@@ -0,0 +1,16 @@
+package com.example.bootproject.system.util;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IpAnalyzer {
+    public static String getClientIp(HttpServletRequest req){
+        String ip = req.getHeader("X-FORWARDED-FOR");
+        if (ip == null) {
+            ip = req.getRemoteAddr();
+        }
+        return ip;
+    }
+}
