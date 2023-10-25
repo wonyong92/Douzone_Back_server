bb8074474b63e6b32003341e2ab3e7c2e9ba12c0
천아현 <fofi7889@naver.com>
2023-10-25 20:12:07 +0900

    getEmployeesInformation 생성

--- src/main/java/com/example/bootproject/controller/rest/admin/AdminController2.java
+++ src/main/java/com/example/bootproject/controller/rest/admin/AdminController2.java
@@ -1,4 +1,13 @@
 package com.example.bootproject.controller.rest.admin;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AdminController2 {
+    @GetMapping("/admin/employee/information")
+    public String getEmployeesInformation() {
+        return "employee_Information";
+    }
+
 }
--- src/main/java/com/example/bootproject/controller/rest/manager/ManagerController2.java
+++ src/main/java/com/example/bootproject/controller/rest/manager/ManagerController2.java
@@ -1,4 +1,9 @@
 package com.example.bootproject.controller.rest.manager;
 
 public class ManagerController2 {
+
+    //연차
+
+
+
 }
