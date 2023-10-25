897b3c2d0ef3a09eaea55e9ea8539b702de3c46a
KyunyeonKim <kgn4746@naver.com>
2023-10-25 20:06:38 +0900

    작성

---
+++ src/main/java/com/example/bootproject/vo/vo1/attendance_info.java
@@ -0,0 +1,17 @@
+package com.example.bootproject.vo.vo1;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class attendance_info {
+    private String attendance_info_id; //근태상태표현저장
+
+    private String employee_id;//사원번호
+
+    private Time start_time;//출근시간
+
+    private Time end_time;//퇴근시간
+
+    private Date attendance_date;//날짜
+
+}
--- src/main/java/com/example/bootproject/vo/vo1/request/RequestDto.java
+++ src/main/java/com/example/bootproject/vo/vo1/request/RequestDto.java
@@ -1,4 +1,13 @@
 package com.example.bootproject.vo.vo1.request;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RequestDto {
+
+    public static class makeAttendanceInfo{
+//안녕
+    }
 }
