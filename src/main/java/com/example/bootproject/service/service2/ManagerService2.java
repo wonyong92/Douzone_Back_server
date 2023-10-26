5eebeaa2cee816a1b71d2d8c84f7405b07a679e3
천아현 <fofi7889@naver.com>
2023-10-26 19:33:13 +0900

    settingWorkTime 메서드 작성 완료

--- src/main/java/com/example/bootproject/service/service2/ManagerService2.java
+++ src/main/java/com/example/bootproject/service/service2/ManagerService2.java
@@ -1,6 +1,7 @@
 package com.example.bootproject.service.service2;
 
 import com.example.bootproject.vo.vo2.response.EmployeeDto;
+import com.example.bootproject.vo.vo2.response.SettingWorkTimeDto;
 import com.example.bootproject.vo.vo2.response.VacationRequestDto;
 
 import java.time.LocalDate;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface ManagerService2 {
     public List<VacationRequestDto> getAllVacationHistory(String date);
     public VacationRequestDto getEmpReqVacationHistory(String employeeId);
+
+    public List<SettingWorkTimeDto> getSettingWorkTime();
 }
