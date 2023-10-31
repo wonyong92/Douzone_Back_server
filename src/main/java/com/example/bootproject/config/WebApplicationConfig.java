b272fee65817e85c5090baeb08bd6f715c6df51a
wonyong92 <ska40806@gmail.com>
2023-10-31 11:17:17 +0900

    [FIX] 단순 이름 변경

--- src/main/java/com/example/bootproject/config/WebApplicationConfig.java
+++ src/main/java/com/example/bootproject/config/WebApplicationConfig.java
@@ -1,6 +1,6 @@
 package com.example.bootproject.config;
 
-import com.example.bootproject.system.interceptor.LoginInterceptor;
+import com.example.bootproject.system.interceptor.LogginInterceptor;
 import com.example.bootproject.system.interceptor.WorkingQueueInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebApplicationConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor());
+        registry.addInterceptor(new LogginInterceptor());
         // TODO : 다른 오쳥에 대해서도 사용할 수 있도록 URI 추가 필요
         registry.addInterceptor(new WorkingQueueInterceptor()).addPathPatterns("/employee/vacation", "/employee/appeal");
         WebMvcConfigurer.super.addInterceptors(registry);
