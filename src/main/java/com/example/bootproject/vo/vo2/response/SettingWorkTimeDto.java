a007de038eef313f6edca2db48f99b9dfd081026
천아현 <fofi7889@naver.com>
2023-11-01 19:16:07 +0900

    스네이크 케이스를 카멜 케이스로 변환, @DateTimeFormat 추가

--- src/main/java/com/example/bootproject/vo/vo2/response/SettingWorkTimeDto.java
+++ src/main/java/com/example/bootproject/vo/vo2/response/SettingWorkTimeDto.java
@@ -1,6 +1,7 @@
 package com.example.bootproject.vo.vo2.response;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -8,13 +9,24 @@ import java.time.LocalTime;
 
 @Data
 public class SettingWorkTimeDto {
-    private int regular_time_adjustment_history_id; //대리키
-    private LocalDate target_date; // 날짜
-    private LocalTime adjusted_start_time; // 조정 출근 시간
-    private LocalTime adjusted_end_time; // 조정 퇴근 시간
+
+    private int regularTimeAdjustmentHistoryId; //대리키
+
+    @DateTimeFormat (pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDate targetDate; // 날짜
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalTime adjustedStartTime; // 조정 출근 시간
+
+    @DateTimeFormat(pattern = "HH:mm:ss")
+    private LocalTime adjustedEndTime; // 조정 퇴근 시간
+
     private String reason; //사유
-    private LocalDateTime regular_time_adjustment_time; // 조정 수행 날짜
-    private String employee_id; // 사원-사원번호
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime regularTimeAdjustmentTime; // 조정 수행 날짜
+
+    private String employeeId; // 사원-사원번호
 
 
 }
