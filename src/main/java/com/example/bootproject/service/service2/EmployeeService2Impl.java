9ef7123b6c732d4d624a849732e549c954bc3756
천아현 <fofi7889@naver.com>
2023-11-01 19:12:35 +0900

    @RequiredArgsConstructor, @Slf4j 추가 및 log 확인 추가

--- src/main/java/com/example/bootproject/service/service2/EmployeeService2Impl.java
+++ src/main/java/com/example/bootproject/service/service2/EmployeeService2Impl.java
@@ -2,29 +2,48 @@ package com.example.bootproject.service.service2;
 
 import com.example.bootproject.repository.mapper.mapper2.EmployeeMapper2;
 import com.example.bootproject.vo.vo2.response.VacationRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class EmployeeService2Impl implements  EmployeeService2{
 
-    @Autowired
-    private EmployeeMapper2 empMapper2;
+    private final EmployeeMapper2 empMapper2;
 
     @Override
     public List<VacationRequestDto> getHistoryOfUsedVacationOfMine(String id) {
-        return empMapper2.getHistoryOfUsedVacationOfMine(id);
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationRequestDto> result = empMapper2.getHistoryOfUsedVacationOfMine(id);
+
+        log.info("empMapper2.getHistoryOfUsedVacationOfMine(id)의 result : {}",result);
+        return result;
     }
 
     @Override
     public List<VacationRequestDto> getHistoryOfRejectedVacationOfMine(String id) {
-        return empMapper2.getHistoryOfRejectedVacationOfMine(id);
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationRequestDto> result =  empMapper2.getHistoryOfRejectedVacationOfMine(id);
+
+        log.info("empMapper2.getHistoryOfRejectedVacationOfMine(id)의 result : {}",result);
+        return result;
     }
 
     @Override
     public List<VacationRequestDto> getHistoryOfUsedVacationOfEmployee(String id) {
-        return empMapper2.getHistoryOfUsedVacationOfEmployee(id);
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationRequestDto> result= empMapper2.getHistoryOfUsedVacationOfEmployee(id);
+
+        log.info("empMapper2.getHistoryOfUsedVacationOfEmployee(id) : {}",result);
+        return result;
     }
 }
--- src/main/java/com/example/bootproject/service/service2/ManagerService2Impl.java
+++ src/main/java/com/example/bootproject/service/service2/ManagerService2Impl.java
@@ -8,37 +8,67 @@ import com.example.bootproject.vo.vo2.response.SettingWorkTimeDto;
 import com.example.bootproject.vo.vo2.response.VacationQuantitySettingDto;
 
 import com.example.bootproject.vo.vo2.response.VacationRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Array;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ManagerService2Impl implements  ManagerService2{
 
-    @Autowired
-    private ManagerMapper2 manMapper2;
+    private final ManagerMapper2 manMapper2;
     @Override
     public List<VacationRequestDto> getAllVacationHistory(String date) {
-        return manMapper2.getAllVacationHistory(date);
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationRequestDto> result = manMapper2.getAllVacationHistory(date);
+
+        log.info("manMapper2.getAllVacationHistory(date)의 result : {}",result);
+        return result;
     }
     @Override
-    public VacationRequestDto getEmpReqVacationHistory(String employeeId) {
-        return manMapper2.getEmpReqVacationHistory(employeeId);
+    public List<VacationRequestDto> getEmpReqVacationHistory(String employeeId) {
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationRequestDto> result = manMapper2.getEmpReqVacationHistory(employeeId);
+
+        log.info("manMapper2.getEmpReqVacationHistory(employeeId)의 result : {}",result);
+        return result;
     }
     @Override
     public List<SettingWorkTimeDto> getSettingWorkTime() {
-        return manMapper2.getSettingWorkTime();
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<SettingWorkTimeDto> result = manMapper2.getSettingWorkTime();
+
+        log.info("manMapper2.getSettingWorkTime()의 result : {}",result);
+        return result;
     }
     @Override
     public List<VacationQuantitySettingDto> getVacationSettingHistory() {
-        return manMapper2.getVacationSettingHistory();
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationQuantitySettingDto> result= manMapper2.getVacationSettingHistory();
+
+        log.info("manMapper2.getVacationSettingHistory()의 result : {}",result);
+        return result;
     }
 
     @Override
     public List<VacationRequestDto> getHistoryOfRejectedVacationOfEmployee(String employeeId) {
-        return manMapper2.getHistoryOfRejectedVacationOfEmployee(employeeId);
+
+        /* result에 어떠한 데이터도 담기지 않으면 null이 아닌 빈 List 형임*/
+        List<VacationRequestDto> result = manMapper2.getHistoryOfRejectedVacationOfEmployee(employeeId);
+
+        log.info("manMapper2.getHistoryOfRejectedVacationOfEmployee(employeeId)의 result : {}",result);
+        return result;
     }
 }
