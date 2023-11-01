1a98dc9c5045ecc80efe31e51f970de0c268a0d0
천아현 <fofi7889@naver.com>
2023-11-01 19:02:15 +0900

    아이디/페이지 넘버/정렬/내림차순 validation 체크 추가 및 권한 확인 api 작성 및 빈 반환값(204 No Content), 정상 반환(200 OK), validation 체크 실패(400 Bad Request), 권한 체크 실패(403 error)의 응답코드 작성 및 @RequiredArgsConstructor,@Slf4j 추가 및 로그 추가 및 페이지네이션 구현

--- src/main/java/com/example/bootproject/controller/rest/admin/AdminController2.java
+++ src/main/java/com/example/bootproject/controller/rest/admin/AdminController2.java
@@ -1,34 +1,106 @@
 package com.example.bootproject.controller.rest.admin;
-
 import com.example.bootproject.service.service2.AdminService2;
 import com.example.bootproject.vo.vo2.response.EmployeeDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.bootproject.vo.vo2.response.Page;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 public class AdminController2 {
 
-    @Autowired
-    private AdminService2 adminService;
+    private final AdminService2 adminService;
+
+    public boolean authCheckApi(){ //권한 확인 API,
+        // 로그인 확인
+        return true;
+    }
+
+    public boolean validationId(String employeeId){ // Id Validation 체크
+        return employeeId.matches("^[0-9]+$"); // 숫자로 구성 되어 있는지 확인
+    }
+
+    public boolean validationPageNum(int currentPage){ //요청 받은 페이지에 대한 validation check
+        return currentPage>0;
+    }
+
+    public boolean validationSort(String getSort){
+        return getSort.matches("^[a-zA-Z_]+$");
+    }
+
+    public boolean validationDesc(String getDescData){
+        return getDescData.matches("^(desc|DESC|)$");
+    }
 
 
-    //전체사원 정보 조회 메서드
+    // 전체 사원 정보 조회 메서드
+    /* TODO : 추후 권한 확인 추가 */
+    /* TODO : 추후 페이지네이션 , 페이지네이션 validation 체크 추가 */
     @GetMapping("/admin/employee/information")
-    public List<EmployeeDto> getEmployeesInformation() {
-        return adminService.getEmpInfo();
+    public ResponseEntity <Page<List<EmployeeDto>>> getEmployeesInformation(@RequestParam(name = "page") String getPageNum, @RequestParam(name="sort", defaultValue = "") String getSort, @RequestParam(name="desc", defaultValue = "") String getDesc) {
+        if(authCheckApi()){
+            //페이징 코드
+            int currentPage = Integer.parseInt(getPageNum); // 쿼리파라미터로 받아온 페이지 번호를 int 형으로 변환
+            if(validationPageNum(currentPage)&&validationSort(getSort)&&validationDesc(getDesc)){
+                // 페이지 번호, Sort 할 컬럼에 대한 validation check
+                /* */
+                Page<List<EmployeeDto>> result = adminService.getEmpInfo(currentPage,getSort,getDesc); // 전체 사원 정보 반환
+                log.info("getEmpInfo result : {}",result);
+                if(result.getData().isEmpty()){
+                    return ResponseEntity.noContent().build(); // 204 No Content
+                }
+                return ResponseEntity.ok(result); // 200 OK
+            }
+            return ResponseEntity.badRequest().build(); //400 Bad Request
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN); // 403 ERROR
+        /*
+        * admin 권한 확인
+        * admin 권한 확인 성공시 ->  전체 사원 정보 반환 받음
+        *                          리턴 받은 데이터가 비어있을 때 : 204 No Content 응답 생성
+        *                          리턴 받은 데이터가 비어있지 않을 때 : 200 응답 받음
+        * admin 권한 확인 실패 ->  403 에러 반환
+        * */
+
     }
 
 
     //특정 사원의 정보 조회 메서드
+    /* TODO : 추후 권한 확인 추가 */
+    /* TODO : 추후 페이지네이션 , 페이지네이션 validation 체크 추가 */
     @GetMapping("/admin/employee/information/{employee_id}")
-    public EmployeeDto getEmployeeInformationByEmployeeId(@PathVariable(name = "employee_id") String employeeId) {
-        return adminService.getOneEmpInfo(employeeId);
+    public ResponseEntity<EmployeeDto> getEmployeeInformationByEmployeeId(@PathVariable(name = "employee_id") String employeeId) {
+        if(authCheckApi()){ //권한 확인 api
+            if(!validationId(employeeId))  //validation 체크
+                return ResponseEntity.badRequest().build(); // 400 Bad Request 반환
+            EmployeeDto result = adminService.getOneEmpInfo(employeeId);
+            log.info("getOneEmpInfo result : {}",result);
+            if(result==null){
+                return ResponseEntity.noContent().build(); //204 No Content 반환
+            }
+            return ResponseEntity.ok(result); // 200 OK, result 객체를 응답 본문으로 반환
+        }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN); //403 error 반환
+        /*
+        * admin 권한 확인
+        * admin 권한 확인 성공시 -> 매개변수 값이 숫자인지 validation 체크
+        *                     ->  validation 체크 성공시 특정 사원의 정보 반환 받음
+        *                         리턴 받은 정보가 null이면 204 No Content 반환
+        *                         리턴 받은 정보가 null이 아니면 : 200 응답 받음
+        *                     ->  validation 체크 실패시 400 Bad Request 반환
+        * admin 권한 확인 실패 시 403 에러 반환
+        * */
     }
+
+
 
 
 
