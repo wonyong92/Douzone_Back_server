0cd9438a092a8d84f9e33278efba13586d947ff0
천아현 <fofi7889@naver.com>
2023-11-01 19:15:09 +0900

    스네이크 케이스를 카멜 케이스로 변환, @DateTimeFormat 추가, vacation_related_start_time, vacation_related_end_time 컬럼 삭제, name 추가

--- src/main/java/com/example/bootproject/vo/vo2/response/VacationRequestDto.java
+++ src/main/java/com/example/bootproject/vo/vo2/response/VacationRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.bootproject.vo.vo2.response;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 
 import java.sql.Time;
@@ -11,17 +12,19 @@ import java.time.LocalTime;
 
 @Data
 public class VacationRequestDto {
-    private int vacation_request_key; //대리키
-    private String vacation_category_key; //연차종류 - 키
-    private String employee_id; // 사원-사원번호
-    private String result; // 신청결과
-    private int vacation_quantity; // 한번에 사용한 연차 개수
-    private LocalDate vacation_start_time; // 연차 시작 날짜
-    private LocalDate vacation_end_time; // 연차 끝 날짜
-    private LocalTime vacation_related_start_time; // 수정 출근 시간
-    private LocalTime vacation_related_end_time; // 수정 퇴근 시간
+    private int vacationRequestKey; //대리키
+    private String vacationCategoryKey; //연차 종류 - 키
+    private String employeeId; // 사원-사원 번호
+    private String vacationRequestStateCategoryKey; //연차 신청 결과
+    private int vacationQuantity; // 한번에 사용한 연차 개수
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate vacationStartDate; // 연차 시작 날짜
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate vacationEndDate; // 연차 끝 날짜
     private String reason; // 사유
-    private LocalDateTime vacation_request_time; // 연차 사용 시청 시간
-    private String reason_for_rejection; // 반려 사유
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime vacationRequestTime; // 연차 사용 신청 시간
+    private String reasonForRejection; // 반려 사유
+    private String name; //사원 이름
 
 }
