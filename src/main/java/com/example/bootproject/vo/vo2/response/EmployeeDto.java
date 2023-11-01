391759384b5ff80b39b1eb96501ef1777f9ac88f
천아현 <fofi7889@naver.com>
2023-11-01 19:11:28 +0900

    스네이크 케이스를 카멜 케이스로 변경, @DateTimeFormat 추가

--- src/main/java/com/example/bootproject/vo/vo2/response/EmployeeDto.java
+++ src/main/java/com/example/bootproject/vo/vo2/response/EmployeeDto.java
@@ -1,15 +1,17 @@
 package com.example.bootproject.vo.vo2.response;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class EmployeeDto {
-    private String employee_id; //사원번호
+    private String employeeId; //사원번호
     private String password; //비밀번호
     private String name; //이름
-    private boolean attendance_manager; //근태 담당자 여부
-    private Date hire_year; // 입사 연도
+    private boolean attendanceManager; //근태 담당자 여부
+    @DateTimeFormat(pattern="yyyy")
+    private Date hireYear; // 입사 연도
 
 }
--- src/main/resources/schema/data_back.sql
+++ src/main/resources/schema/data_back.sql
@@ -3,15 +3,31 @@
 -- insert into vacation_category values("a",2);
 -- insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason) values("a","123","a",1,"2023-01-01","2023-01-10","09:00:00","09:00:00","2023-01-01 18:00:00","a");
 
-insert into employee values ('testid','testpw','name1',false,'2023-01-01');
-insert into employee values ('testid2','testpw2','name2',false,'2023-01-01');
-insert into vacation_category values("a",2);
-insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason) values("a","testid","승인",1,"2023-01-01","2023-01-10","2023-01-11 09:00:00","2023-01-11 18:00:00","2023-01-01 18:00:00","a");
-insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason) values("a","testid","승인",1,"2023-02-01","2023-02-10","2023-02-11 09:00:00","2023-02-11 18:00:00","2023-02-01 18:00:00","a");
-insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason) values("a","testid","승인",1,"2023-03-01","2023-03-10","2023-03-11 09:00:00","2023-03-11 18:00:00","2023-03-01 18:00:00","a");
-insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason) values("a","testid","승인",1,"2023-04-01","2023-04-10","2023-04-11 09:00:00","2023-04-11 18:00:00","2023-04-01 18:00:00","a");
-insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason,reason_for_rejection) values("a","testid","반려",1,"2023-01-02","2023-01-11","2023-01-12 09:00:00","2023-01-12 18:00:00","2023-01-02 18:00:00","a","안돼 돌아가");
-insert into vacation_request(vacation_category_key, employee_id, result, vacation_quantity, vacation_start_time, vacation_end_time, vacation_related_start_time, vacation_related_end_time, vacation_request_time, reason,reason_for_rejection) values("a","testid2","반려",1,"2023-01-02","2023-01-11","2023-01-12 09:00:00","2023-01-12 18:00:00","2023-01-02 18:00:00","a","안돼 돌아가세요");
-insert into regular_time_adjustment_history(target_date,adjusted_start_time,adjusted_end_time,reason,regular_time_adjustment_time,employee_id) values("2023-01-01","2023-01-01 11:00:00","2023-01-01 12:00:00","이유","2023-01-01 10:00:00","testid");
-insert into vacation_quantity_setting(freshman,senior,setting_time,employee_id) values(11,14,"2023-10-26 09:00:00","testid");
+insert into employee values ("1234",'testpw','name1',false,'2023-01-01');
+insert into employee values ("5678",'testpw2','name2',false,'2023-01-01');
+insert into employee values ("234",'testpw3','name3',false,'2023-01-01');
+insert into employee values ("345",'testpw4','name4',false,'2023-01-01');
+insert into employee values ("456",'testpw5','name5',false,'2023-01-01');
+insert into employee values ("567",'testpw6','name6',false,'2023-01-01');
+insert into employee values ("678",'testpw7','name7',false,'2023-01-01');
+insert into employee values ("789",'testpw8','name8',false,'2023-01-01');
+insert into employee values ("890",'testpw9','name9',false,'2023-01-01');
+insert into employee values ("4567",'testpw10','name10',false,'2023-01-01');
+insert into employee values ("651",'testpw11','name11',false,'2023-01-01');
 
+
+
+
+insert into vacation_category values("a",2);
+insert into vacation_request_state_category values ("승인");
+insert into vacation_request_state_category values ("반려");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason) values("a","1234","승인",1,"2023-01-01","2023-01-10","2023-01-01 15:00:00","a");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason) values("a","5678","승인",1,"2023-01-01","2023-01-10","2023-01-01 16:00:00","a");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason) values("a","1234","승인",1,"2023-03-01","2023-03-10","2023-03-01 15:00:00","a");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason) values("a","5678","승인",1,"2023-03-01","2023-03-10","2023-03-01 15:00:00","a");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason,reason_for_rejection) values("a","1234","반려",1,"2023-05-01","2023-05-10","2023-05-01 15:00:00","a","안돼 돌아가");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason,reason_for_rejection) values("a","5678","반려",1,"2023-05-01","2023-05-10","2023-05-01 15:00:00","a","안돼 돌아가요");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason,reason_for_rejection) values("a","1234","반려",1,"2023-06-01","2023-06-10","2023-06-01 15:00:00","a","안돼 돌아가");
+insert into vacation_request(vacation_category_key, employee_id, vacation_request_state_category_key, vacation_quantity, vacation_start_date, vacation_end_date,  vacation_request_time, reason,reason_for_rejection) values("a","5678","반려",1,"2023-07-01","2023-07-10","2023-07-01 15:00:00","a","안돼 돌아가요");
+insert into vacation_quantity_setting(freshman,senior,setting_time,employee_id) values(11,14,"2023-10-26 09:00:00","1234");
+INSERT INTO regular_time_adjustment_history(TARGET_DATE, adjusted_start_time,adjusted_end_time,reason,regular_time_adjustment_time,employee_id) values("2023-01-01","09:00:00","16:00:00","집에 일찍 가고싶어서","2022-12-31 09:00:00","1234");
