9210fc0e2692728e44daf2a5747839dad487a0d9
천아현 <fofi7889@naver.com>
2023-11-01 19:15:27 +0900

    스네이크 케이스를 카멜 케이스로 변환, @DateTimeFormat 추가

--- src/main/java/com/example/bootproject/vo/vo2/response/VacationQuantitySettingDto.java
+++ src/main/java/com/example/bootproject/vo/vo2/response/VacationQuantitySettingDto.java
@@ -1,15 +1,23 @@
 package com.example.bootproject.vo.vo2.response;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 @Data
 public class VacationQuantitySettingDto {
-    private int settind_key; //대리키
+    private int settingKey; //대리키
+
     private int freshman; //1년 미만 - 개수
+
     private int senior; //1년 이상 - 개수
-    private LocalDateTime setting_time; // 설정 시간
-    private LocalDate target_date; // 적용 날짜
-    private String employee_id; //사원-사원번호
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime settingTime; // 설정 시간
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate targetDate; // 적용 날짜
+
+    private String employeeId; //사원-사원번호
 }
