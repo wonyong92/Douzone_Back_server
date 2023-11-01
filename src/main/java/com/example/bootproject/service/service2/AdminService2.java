a749b965147a5b84a70965e6ec7f1ce0a1238c05
천아현 <fofi7889@naver.com>
2023-11-01 19:09:14 +0900

    getEmpInfo()을 Page로 감싸서 수정

--- src/main/java/com/example/bootproject/service/service2/AdminService2.java
+++ src/main/java/com/example/bootproject/service/service2/AdminService2.java
@@ -1,11 +1,12 @@
 package com.example.bootproject.service.service2;
 
 import com.example.bootproject.vo.vo2.response.EmployeeDto;
+import com.example.bootproject.vo.vo2.response.Page;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface AdminService2 {
-    public List<EmployeeDto> getEmpInfo();
+    public Page<List<EmployeeDto>> getEmpInfo(int currentPage, String getSort, String desc);
     public EmployeeDto getOneEmpInfo(String employeeId);
 }
