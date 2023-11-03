b3e13c5752dd2aee48a7a320fbccd4b31e523188
천아현 <fofi7889@naver.com>
2023-11-03 14:27:16 +0900

    [FIX] employeeID 필드에 @JsonIgnore 추가 및 generated key 저장하는 필드 생성

--- src/main/java/com/example/bootproject/vo/vo2/request/DefaultVacationRequestDto.java
+++ src/main/java/com/example/bootproject/vo/vo2/request/DefaultVacationRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.bootproject.vo.vo2.request;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -11,10 +12,14 @@ import java.time.LocalDateTime;
 public class DefaultVacationRequestDto {
     private int freshman; //1년 미만일 때 개수
     private int senior; //1년 이상일 때 개수
-    private LocalDateTime settingTime; // 설정 시간
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate targetDate; // 대상 날짜
+
+    @JsonIgnore
     private String employeeId; // 작성 근태 담당자의 사원 번호
+
+    private int settingKey; // generated key 저장
+
 
 }
